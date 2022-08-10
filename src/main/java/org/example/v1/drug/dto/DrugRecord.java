e42ce9f2bab9728e1e799afee76f68d482a297a1
Zuzulevs <zhe8745@yandex.ru>
2022-08-10 19:51:14 +0400

    refactroing DrugRecord (add lombok annotation)

--- src/main/java/org/example/v1/drug/dto/DrugRecord.java
+++ src/main/java/org/example/v1/drug/dto/DrugRecord.java
@@ -1,6 +1,8 @@
 package org.example.v1.drug.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
@@ -8,55 +10,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
 public class DrugRecord {
 
-    private Integer id;
+    private Long id;
+
     private String name;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private LocalDate date;
+    private LocalDate before_date;
 
     private String instruction;
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getInstruction() {
-        return instruction;
-    }
-
-    public void setInstruction(String instruction) {
-        this.instruction = instruction;
-    }
-
-    @Override
-    public String toString() {
-        return "Drug{" +
-                "name='" + name + '\'' +
-                ", date='" + date + '\'' +
-                ", instruction='" + instruction + '\'' +
-                '}';
-    }
 }
