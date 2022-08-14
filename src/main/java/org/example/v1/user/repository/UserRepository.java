180a5b9ddd1202cedd049059593c40111405268e
Zuzulevs <zhe8745@yandex.ru>
2022-08-14 23:12:16 +0400

    Add userRepository for user entity

---
+++ src/main/java/org/example/v1/user/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package org.example.v1.user.repository;
+
+import org.example.v1.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
