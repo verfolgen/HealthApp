9946e3cb67a10168056c0ee587d4773b4d854dbc
Zuzulevs <zhe8745@yandex.ru>
2022-08-14 23:13:11 +0400

    Add UserService for manage entity User

---
+++ src/main/java/org/example/v1/user/service/UserService.java
@@ -0,0 +1,23 @@
+package org.example.v1.user.service;
+
+import lombok.AllArgsConstructor;
+import org.example.v1.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private static final String USER_NOT_FOUND_MSG = "User with email %s not found";
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+    }
+}
