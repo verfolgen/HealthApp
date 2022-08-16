b531b4eb262c89e93c149120b887c2a98413e17b
Zuzulevs <zhe8745@yandex.ru>
2022-08-16 14:19:37 +0400

    refactoring user

--- src/main/java/org/example/v1/user/entity/User.java
+++ src/main/java/org/example/v1/user/entity/User.java
@@ -18,10 +18,10 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "firstName")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "lastName")
+    @Column(name = "last_name")
     private String lastName;
 
     @Column(name = "email")
@@ -30,7 +30,7 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
-    @Column(name = "userRole")
+    @Column(name = "user_role")
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
--- src/main/java/org/example/v1/user/service/UserService.java
+++ src/main/java/org/example/v1/user/service/UserService.java
@@ -1,23 +1,58 @@
 package org.example.v1.user.service;
 
 import lombok.AllArgsConstructor;
+import org.example.v1.registration.token.ConfirmationToken;
+import org.example.v1.registration.token.ConfirmationTokenRepository;
+import org.example.v1.registration.token.ConfirmationTokenService;
+import org.example.v1.user.entity.User;
 import org.example.v1.user.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class UserService implements UserDetailsService {
-
     private static final String USER_NOT_FOUND_MSG = "User with email %s not found";
-
+    private final ConfirmationTokenService confirmationTokenService;
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+    }
+
+    public String signUp(User user) {
+        boolean userExists = userRepository
+                .findByEmail(user.getEmail())
+                .isPresent();
+
+        if(userExists) {
+            throw new IllegalStateException(user.getEmail() + " is exist");
+        }
+
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+
+        String token = UUID.randomUUID().toString();
+
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                user
+        );
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+        //TODO:SEND EMAIL
+
+        return token;
     }
 }
