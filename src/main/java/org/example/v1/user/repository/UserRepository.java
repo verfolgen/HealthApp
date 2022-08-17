d61020a157239bb55f0ce814d2e538e6ddbd850e
Zuzulevs <zhe8745@yandex.ru>
2022-08-17 14:58:50 +0400

    add UserService

--- src/main/java/org/example/v1/user/repository/UserRepository.java
+++ src/main/java/org/example/v1/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package org.example.v1.user.repository;
 
 import org.example.v1.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +13,9 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User a SET a.enabled = TRUE WHERE a.email = ?1")
+    int enableUser(String email);
 }
