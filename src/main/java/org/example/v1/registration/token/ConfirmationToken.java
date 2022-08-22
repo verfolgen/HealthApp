e655b3b1851b7f7b33a3b385f70d56df2cb076a2
Zuzulevs <zhe8745@yandex.ru>
2022-08-22 10:13:47 +0400

    add swagger specification for entities

--- src/main/java/org/example/v1/registration/token/ConfirmationToken.java
+++ src/main/java/org/example/v1/registration/token/ConfirmationToken.java
@@ -1,33 +1,46 @@
 package org.example.v1.registration.token;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.v1.user.entity.User;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
+@Schema(description = "Entity for store data for confirmation token")
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "token")
 public class ConfirmationToken {
+    @Schema(description = "Id of token")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Schema(description = "Issued token")
     @Column(name = "token")
+    @NotBlank
+    @Max(255)
     private String token;
 
+    @Schema(description = "Created date of the token")
     @Column(name = "created_at")
+    @NotBlank
     private LocalDateTime createdAt;
 
+    @Schema(description = "Expire date of the token")
     @Column(name = "expires_at")
     private LocalDateTime expiresAt;
 
+    @Schema(description = "Confirm date of the token")
     @Column(name = "confirmed_at")
     private LocalDateTime confirmedAt;
 
+    @Schema(description = "The user associated with the token")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
